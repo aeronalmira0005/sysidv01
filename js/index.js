2c3460c46dcf9b3940931e222750355d13d4bf18
aeronalmira0005 <69041811+aeronalmira0005@users.noreply.github.com>
2020-10-22 02:31:48 +0000

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
-$(document).ready(function() {
-
-  setTimeout(function(){
-    $('.container-grayarrow1-img .grayarrow1-cover').css({"animation-name":"grayarrow1", "width":"0"});
-    $('.container-grayarrow2-img .grayarrow2-cover').css({"animation-name":"grayarrow2", "height":"0"});
-    $('.container-grayarrow3-img .grayarrow3-cover').css({"animation-name":"grayarrow3", "width":"0"});
-  },1000);
-});
+// $(document).ready(function() {
+//
+//   setTimeout(function(){
+//     $('.container-grayarrow1-img .grayarrow1-cover').css({"animation-name":"grayarrow1", "width":"0"});
+//     $('.container-grayarrow2-img .grayarrow2-cover').css({"animation-name":"grayarrow2", "height":"0"});
+//     $('.container-grayarrow3-img .grayarrow3-cover').css({"animation-name":"grayarrow3", "width":"0"});
+//   },1000);
+// });
