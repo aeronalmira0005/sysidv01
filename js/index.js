1a1627a1ad6e0c4da6d9d1aae809076fa2df11a7
aeronalmira0005 <69041811+aeronalmira0005@users.noreply.github.com>
2020-10-27 17:35:15 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,28 +1,28 @@
-// // Get the modal
-// var emailModal = document.getElementById("email-modal");
-//
-// // Get the button that opens the modal
-// var emailModalButton = document.getElementById("email-modal-btn");
-//
-// // Get the <span> element that closes the modal
-// var closeEmailModalButton = document.getElementsByClassName("close-email-modal")[0];
-//
-// // When the user clicks on the button, open the modal
-// emailModalButton.onclick = function() {
-//   emailModal.style.display = "block";
-// }
-//
-// // When the user clicks on <span> (x), close the modal
-// closeEmailModalButton.onclick = function() {
-//   emailModal.style.display = "none";
-// }
-//
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == emailModal) {
-//     emailModal.style.display = "none";
-//   }
-// }
+// Get the modal
+var emailModal = document.getElementById("email-modal");
+
+// Get the button that opens the modal
+var emailModalButton = document.getElementById("email-modal-btn");
+
+// Get the <span> element that closes the modal
+var closeEmailModalButton = document.getElementsByClassName("close-email-modal")[0];
+
+// When the user clicks on the button, open the modal
+emailModalButton.onclick = function() {
+  emailModal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+closeEmailModalButton.onclick = function() {
+  emailModal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == emailModal) {
+    emailModal.style.display = "none";
+  }
+}
 
 $(".container-mail-connect-text").click(function(){
   $(".container-mail-connect-text a")[0].click();
